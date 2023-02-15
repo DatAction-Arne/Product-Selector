f332caedaf684cf66eeb56e054feaa646df21863
DatAction-Arne <96430642+DatAction-Arne@users.noreply.github.com>
2023-02-15 11:49:00 +0100

    init commit

--- server.py
+++ server.py
@@ -2,74 +2,32 @@ import traceback
 import uuid
 import sys
 
-import flask
+from flask import Flask, render_template, url_for, request, redirect, session, make_response, send_from_directory
 import flask.json
 import gevent.pywsgi
-import pymysql
-import redis
-
-from platformshconfig import Config
-
-app = flask.Flask(__name__)
-config = Config()
 
 
-@app.route('/')
-def root():
-    tests = {
-        "database": wrap_test(test_mysql, config.credentials("database")),
-        "redis": wrap_test(test_redis, config.credentials("rediscache"))
-    }
-    return flask.json.jsonify(tests)
+def get_abslute_path(subfolder, path):
+    if subfolder is None:
+        return app.root_path
+    else:
+        return app.root_path + subfolder + path
 
 
-def wrap_test(callback, *args, **kwargs):
-    try:
-        result = callback(*args, **kwargs)
-        return {
-            "status": "OK",
-            "return": result,
-        }
-    except Exception:
-        return {
-            "status": "ERROR",
-            "error": traceback.format_exception(*sys.exc_info())
-        }
+
+app = Flask(__name__)
+app.jinja_env.globals.update(get_abslute_path=get_abslute_path)
+#app.config["SECRET_KEY"] = secrets.token_urlsafe(7)
 
 
-def test_mysql(instance):
-    connection = pymysql.connect(
-        host=instance["host"],
-        port=instance["port"],
-        user=instance["username"],
-        password=instance["password"],
-        db=instance["path"],
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor,
-    )
+@app.route("/")
 
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT 1")
-            cursor.fetchone()
-
-    finally:
-        connection.close()
-
-
-def test_redis(instance):
-    r = redis.StrictRedis(
-        host=instance["host"],
-        port=instance["port"],
-        db=0,
-    )
-    key_name = "foo-%s" + str(uuid.uuid4())
-    value = b"bar"
-
-    r.set(key_name, "bar")
-    assert value == r.get(key_name)
+@app.route("/login")
+def login():
+    return "Not yet implemented."
 
 
 if __name__ == "__main__":
-    http_server = gevent.pywsgi.WSGIServer(('127.0.0.1', int(config.port)), app)
-    http_server.serve_forever()
+    #http_server = gevent.pywsgi.WSGIServer(('127.0.0.1', int(config.port)), app)
+    #http_server.serve_forever()
+    app.run(debug=True)
